924174deda0826b7e79e20c117b008c40f2e8967
R.G.J Janssen <rgj_janssen@live.nl>
2016-07-04 14:27:37 +0200

    v0.1 Toegevoegt:
    Form1:
    -Knoppen voor aanpassen en verwijderen van gerechten
    -Textboxen om data van gerecht aan te passen
    
    Database Class:
    -Verwijder query
    -Pas aan query
    
    Form1 class:
    -Code voor het verwijderen van een gerecht
    -Code voor het aanpassen van een gerecht

---
+++ KerstDinerJanssen/KerstDinerFamilieJanssen/Database.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace KerstDinerFamilieJanssen
+{
+    public class Database
+    {
+        string connStr = "server=80.100.207.50:3306;user=richard;database=kerstdb;port=3306;password=rgjj4kerst;";
+        MySqlConnection myConnection;
+        Gerecht gerecht;
+        //80.100.207.50:3306
+        private string Query;
+        
+        //Alle data inserten in de database
+        public void InsertData(int id, string naam, string bereidvoor, string soorteten)
+        {
+            try
+            {
+                Query = "INSERT INTO Gerecht (ID, Naam, BereidVoor, SoortEten) " + "Values (" + id + ", '" + naam + "', '" + bereidvoor + "', '" + soorteten + "')";
+                MySqlCommand myCommand = new MySqlCommand(Query, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
+        }
+
+        //Alle gerechten ophalen
+        public List<Gerecht> GetGerechten()
+        {
+            List<Gerecht> gerechten = new List<Gerecht>();
+            Query = "SELECT * FROM Gerecht";
+
+            gerechten.Clear();
+
+            try
+            {
+                MySqlDataReader myReader = null;
+                MySqlCommand myCommand = new MySqlCommand(Query, myConnection);
+                myReader = myCommand.ExecuteReader();
+                using (myReader)
+                {
+                    while (myReader.Read())
+                    {
+                        gerecht = new Gerecht(myReader["Naam"].ToString(), myReader["BereidVoor"].ToString(), myReader["SoortEten"].ToString());
+                        gerechten.Add(gerecht);
+                    }
+                }
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+
+            return gerechten;
+        }
+
+        //Haal het hoogste ID nummer op
+        public int GetMaxID()
+        {
+            int result = 0;
+            Query = "SELECT MAX(ID) AS ID FROM Gerecht";
+
+            try
+            {
+                MySqlDataReader myReader = null;
+                MySqlCommand myCommand = new MySqlCommand(Query, myConnection);
+                myReader = myCommand.ExecuteReader();
+                using (myReader)
+                {
+                    while (myReader.Read())
+                    {
+                        result = Convert.ToInt32(myReader["ID"]);
+                    }
+                }   
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            return result;
+        }
+
+        //Verwijder een gerecht
+        public void VerwijderGerecht(string naam, string bereidvoor, string soorteten)
+        {
+            Query = "DELETE FROM Gerecht WHERE Naam = " + naam + " AND BereidVoor = " + bereidvoor + " AND SoortEten = " + soorteten;
+
+            try
+            {
+                MySqlCommand myCommand = new MySqlCommand(Query, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void PasGerechtAan(string naam, string bereidvoor, string soorteten, string nieuwnaam, string nieuwbereidvoor, string nieuwsoorteten)
+        {
+            Query = "UPDATE Gerecht SET Naam = " + nieuwnaam + ", BereidVoor = " + nieuwbereidvoor + ", SoortEten = " + nieuwsoorteten + " WHERE Naam = " + naam + " AND BereidVoor = " + bereidvoor + " AND SoortEten = " + soorteten;
+
+            try
+            {
+                MySqlCommand myCommand = new MySqlCommand(Query, myConnection);
+                myCommand.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void ConnectToDB()
+        {
+            try
+            {
+                myConnection = new MySqlConnection();
+                myConnection.ConnectionString = connStr;
+                myConnection.Open();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void DisconnectFromDB()
+        {
+            try
+            {
+                myConnection.Close();
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+    }
+}
---
+++ KerstDinerJanssen/KerstDinerFamilieJanssen/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace KerstDinerFamilieJanssen
+{
+    public partial class Form1 : Form
+    {
+        Database db = new Database();
+
+        List<Gerecht> gerechten = new List<Gerecht>();
+
+        string naam;
+        string bereidVoor;
+        string soortEten;
+        string geselecteerdGerecht;
+        string[] stringArray;
+        int ID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateListboxes();
+        }
+
+        private void btnInschrijven_Click(object sender, EventArgs e)
+        {
+            db.ConnectToDB();
+            //Genereer ID
+            ID = db.GetMaxID() + 1;
+            
+            //Als iets niet is ingevuld
+            if(tbVoornaam.Text == "")
+            {
+                MessageBox.Show("Voornaam is niet ingevuld");
+            }
+            else if(tbAchternaam.Text == "")
+            {
+                MessageBox.Show("Achternaam is niet ingevuld");
+            }
+            else if(tbBereidVoor.Text == "")
+            {
+                MessageBox.Show("Bereid voor is niet ingevuld");
+            }
+            else if(cbSoortEten.Text == "")
+            {
+                MessageBox.Show("Soort eten is niet ingevuld");
+            }
+            //Anders data in database toevoegen
+            else
+            {
+                naam = Convert.ToString(tbVoornaam.Text) + " " + Convert.ToString(tbAchternaam.Text);
+                bereidVoor = Convert.ToString(tbBereidVoor.Text);
+                soortEten = Convert.ToString(cbSoortEten.Text);
+                db.InsertData(ID, naam, bereidVoor, soortEten);
+
+                UpdateListboxes();
+            }
+
+            db.DisconnectFromDB();
+        }
+
+        public void UpdateListboxes()
+        {
+            lbWatWordtVoorbereid.Items.Clear();
+            db.ConnectToDB();
+
+            gerechten = db.GetGerechten();
+
+            //Alle gerechten worden in de listbox gezet
+            foreach (Gerecht g in gerechten)
+            {
+                lbWatWordtVoorbereid.Items.Add(g);
+            }
+
+            db.DisconnectFromDB();
+        }
+
+        //Verwijder het geselecteerde gerecht
+        private void btnVerwijderGerecht_Click(object sender, EventArgs e)
+        {
+            geselecteerdGerecht = Convert.ToString(lbWatWordtVoorbereid.SelectedItem);
+            stringArray = new string[3];
+            stringArray = geselecteerdGerecht.Split(' ');
+
+            naam = stringArray[0];
+            bereidVoor = stringArray[1];
+            soortEten = stringArray[2];
+
+            db.VerwijderGerecht(naam, bereidVoor, soortEten);
+            UpdateListboxes();
+        }
+
+        //Pas het gerecht aan
+        private void btnPasGerechtAan_Click(object sender, EventArgs e)
+        {
+            geselecteerdGerecht = Convert.ToString(lbWatWordtVoorbereid.SelectedItem);
+            stringArray = new string[3];
+            stringArray = geselecteerdGerecht.Split(' ');
+
+            //Originele data
+            naam = stringArray[0];
+            bereidVoor = stringArray[1];
+            soortEten = stringArray[2];
+
+            //Voor aan te passen
+            tbNieuwNaam.Text = stringArray[0];
+            tbNieuwBereidVoor.Text = stringArray[1];
+            tbNieuwSoortEten.Text = stringArray[2];
+        }
+
+        //Pas het gerecht daadwerkelijk aan
+        private void btnPasAan_Click(object sender, EventArgs e)
+        {
+            db.PasGerechtAan(naam, bereidVoor, soortEten, tbNieuwNaam.Text, tbNieuwBereidVoor.Text, tbNieuwSoortEten.Text);
+            UpdateListboxes();
+        }
+    }
+}
